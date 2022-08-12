7a01227890bbe93160703eef6a96983a24ef7130
JohnKamaujk <johnjkkamau@gmail.com>
2022-08-12 13:11:36 +0300

    Added js & all selection objects array

---
+++ script.js
@@ -0,0 +1,17 @@
+const selectionButtons = document.querySelectorAll("[data-selection]");
+const SELECTIONS = [
+  { name: "rock", emoji: "✊🏿", beats: "scissors" },
+  { name: "paper", emoji: "✋🏿", beats: "rock" },
+  { name: "scissors", emoji: "✌🏿", beats: "paper" },
+];
+
+selectionButtons.forEach((selectionButton) => {
+  selectionButton.addEventListener("click", (e) => {
+    const selectionName = selectionButton.dataset.selection;
+    makeSelection(selectionName);
+  });
+});
+
+const makeSelection = (selection) => {
+  console.log(selection);
+};
